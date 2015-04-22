00d6e6bc478c94f4a19697e2d1d03766a697c83a
Sakkeer Hussain <sakkeerhussainp@gmail.com>
2015-04-22 09:32:02 +0530

    added todays sales report

--- controller/api/menu_list.php
+++ controller/api/menu_list.php
@@ -26,7 +26,7 @@ if(isset($_SESSION['user_id']) and !empty($_SESSION['user_id'])){
             array('menu_item_name'=>'Bank Deposite','menu_item_id'=>'4'),
             array('menu_item_name'=>'Expence','menu_item_id'=>'5'),
             array('menu_item_name'=>'Customer','menu_item_id'=>'6'),
-            array('menu_item_name'=>'Stock','menu_item_id'=>'16'));
+            array('menu_item_name'=>'Add to stock','menu_item_id'=>'16'));
         
         $responce = array('status'=>'success','error'=>'','data'=> array('menu_list'=>$menu_list));
     }else if($user->user_type_id==2){  
