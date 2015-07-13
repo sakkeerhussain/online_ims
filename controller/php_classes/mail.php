29c910c58b48ba3a433e7d66a55fb71d6b8dedc4
Sakkeer Hussain <sakkeerhussainp@gmail.com>
2015-07-13 23:35:39 +0530

    Updated error reporting

--- controller/php_classes/mail.php
+++ controller/php_classes/mail.php
@@ -9,31 +9,76 @@
  * @author Sakkeer Hussain
  */
 class mail {
-    public $to = 'cgtsoft@gmail.com, sakkeerhussainp@gmail.com';
+    public $to = 'cgtsoft@gmail.com, sakkeerhussainp@gmail.com, piknikdates@gmail.com';
     public $subject = 'Error Reporting from Pik Nik IMS';
     public $header = 'From: webmaster@piknikindia.in';
     public $tag = "MAIL CONTROLLER";
     public function send_error_mail($query, $error){
+        
+        ob_start();
+        echo print_r($_SESSION);
+        $session = ob_get_clean();
+        ob_start();
+        echo print_r($_POST);
+        $post = ob_get_clean();
+        ob_start();
+        echo print_r($_GET);
+        $get = ob_get_clean();
+        ob_start();
+        echo print_r($_SERVER);
+        $server = ob_get_clean();
+        
+        
         $message = '
             <html>
             <head>
               <title style="font-size:28px;">'.$this->subject.'</title>
             </head>
-            <body style="background-color:#21acd7; color:#fff;">
-              <p>'.$this->subject.'</p>
-              <table>
+            <body style="background-color:#fff; color:#21acd7; border : 5px solid #21acd7;">
+              <div style="border : 5px solid #21acd7; margin :0; padding :20px; text-align : centre;">
+                <h2>'.$this->subject.'</h2>
+              </div>
+              <table style="border : 5px solid #21acd7; padding :20px; ">
                 <tr>
                   <td style="font-size:25px;">Query</td>
                 </tr>
                 <tr>
                   <td>'.$query.'</td>
                 </tr>
-                <tr><td><hr/></td></tr>
+                <tr><td  style="background-color: #21acd7; height:5px;"></td></tr>
                 <tr>
-                  <td style="font-size:25px;">Error</td>
+                  <td style="font-size:25px; padding-top : 20px;">Error</td>
                 </tr>
                 <tr>
                   <td>'.$error.'</td>
+                </tr>
+                <tr><td  style="background-color: #21acd7; height:5px;"></td></tr>
+                <tr>
+                  <td style="font-size:25px; padding-top : 20px;">Session</td>
+                </tr>
+                <tr>
+                  <td>'.$session.'</td>
+                </tr>
+                <tr><td  style="background-color: #21acd7; height:5px;"></td></tr>
+                <tr>
+                  <td style="font-size:25px; padding-top : 20px;">Post</td>
+                </tr>
+                <tr>
+                  <td>'.$post.'</td>
+                </tr>
+                <tr><td  style="background-color: #21acd7; height:5px;"></td></tr>
+                <tr>
+                  <td style="font-size:25px; padding-top : 20px;">Get</td>
+                </tr>
+                <tr>
+                  <td>'.$get.'</td>
+                </tr>
+                <tr><td  style="background-color: #21acd7; height:5px;"></td></tr>
+                <tr>
+                  <td style="font-size:25px; padding-top : 20px;">Server</td>
+                </tr>
+                <tr>
+                  <td>'.$server.'</td>
                 </tr>
               </table>
             </body>
@@ -45,6 +90,12 @@ class mail {
         $result = mail($this->to, $this->subject, $message, $headers);
         $description = "mail sent, query : $query, error : $error, result : $result ";
         Log::i($this->tag, $description);
+        
+        
+        //saving to an html file
+//        $h = fopen("sample.html", "w");
+//        fwrite($h, $message);
+//        fclose($h);
     }
 }
 
