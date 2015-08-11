0a465be7e59aa4ee8f9d454e0449ede104402d04
Sakkeer Hussain <sakkeerhussainp@gmail.com>
2015-08-11 23:43:08 +0530

    renamed sales local sync task

--- sales_local_sync_task/sales_sync_task.php
+++ sales_local_sync_task/sales_sync_task.php
@@ -4,17 +4,17 @@ define("BASE_URL", "http://localhost/piknik_ims/controller/api/");
 $user_id = 0;
 
 if(is_loggedin()){
-    sync_stock_report();
+    send_sales_to_server();
     if($user_id==0){ //session expired
         if(login()){
-            sync_stock_report();
+            send_sales_to_server();
         }else{
             send_error_message("");
         }
     }
 }else{
     if(login()){
-        sync_stock_report();
+        send_sales_to_server();
     }else{
         send_error_message("");
     }
@@ -48,36 +48,60 @@ function is_loggedin(){
     }
 }
 
-function sync_stock_report(){
+function send_sales_to_server(){
     global $user_id;
-    $tag = 'STOCK SYNC';
-    $ch = curl_init(BASE_URL.'get_form_data.php?user_id='.$user_id);
-    $post_params = array('form_id'=>9);
-    curl_setopt ($ch, CURLOPT_COOKIEJAR, COOKIE_FILE); 
-    curl_setopt ($ch, CURLOPT_COOKIEFILE, COOKIE_FILE);     
-    curl_setopt($ch, CURLOPT_POST, 1);
-    curl_setopt($ch, CURLOPT_POSTFIELDS,  http_build_query($post_params));                
-    curl_setopt ($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_HEADER, FALSE);
-    $content = curl_exec ($ch);    
-    curl_close($ch);
-    $json = json_decode($content, TRUE);
+    $tag = 'SALES SYNC';
+    if(mysql_connect("localhost", "root", "") and mysql_select_db("sample")){
+        while (TRUE){
+            $query = "SELECT * FROM `sales` WHERE `synced` = 0 Limit 1";
+            $result = mysql_query($query);
+            if ($row = mysql_fetch_assoc($result)) {
+                
+                $amount = $row["amount"];
+                $customer_id = $row["customer_id"];
+                $discount = $row["discount"];
+                
+                $ch = curl_init(BASE_URL.'add_form_data.php?user_id='.$user_id);
+                
+                $item = array('id'=>1,'quantity'=>1,'rate'=>1,'tax'=>1,'discount'=>1);
+                $items = array($item, $item, $item);
+                $fields = array('form_id'=>1, 'customer_id'=>$customer_id,'total'=>$amount,'net_amount'=>$amount,'tax_amount'=>0,'discount'=>$discount,'items'=>$items);
+                
+                Log::i($tag, "Sale syncing with server, sale : ".  multi_implode(" ", $fields));
+                curl_setopt ($ch, CURLOPT_COOKIEJAR, COOKIE_FILE); 
+                curl_setopt ($ch, CURLOPT_COOKIEFILE, COOKIE_FILE);     
+                curl_setopt($ch, CURLOPT_POST, 1);
+                curl_setopt($ch, CURLOPT_POSTFIELDS,  http_build_query($fields));                
+                curl_setopt ($ch, CURLOPT_RETURNTRANSFER, true);
+                curl_setopt($ch, CURLOPT_HEADER, FALSE);
+                $content = curl_exec ($ch);    
+                curl_close($ch);
+                $json = json_decode($content, TRUE);
 
-    echo "<hr/>STOCK SYNC<br/>";
-    print_r($json);
-    echo "<hr/>";
+                echo "<hr/>SALE SYNC<br/>";
+                print_r($json);
+                echo "<hr/>";
                 
-    Log::i($tag, "Stock sync Responce : ".  multi_implode(" ", $json));
+                Log::i($tag, "Responce : ".  multi_implode(" ", $json));
                 
-    $status = $json['status'];
-    if($status == 'success'){
-        write_plu_file($json['data']['stock']);
-    }else{
-        if($json['error'] == 'Session expired'){
-            $user_id = 0;
-        }else{
-            send_error_message(mysql_error());
+                $status = $json['status'];
+                if($status == 'success'){
+                    $sale_id = $json['data']['id'];
+                    $query = "UPDATE `sales` SET `synced`=1,`sale_id`='$sale_id' WHERE `id` = '".$row["id"]."'";
+                    mysql_query($query);
+                }else{ // response error
+                    if($json['error'] == 'Session expired'){
+                        $user_id = 0;
+                    }
+                    break;
+                }
+                
+            } else { //no sale left to sync
+                break;
+            }
         }
+    }else{
+        send_error_message(mysql_error());
     }
 }
 
@@ -86,11 +110,14 @@ function send_error_message($error_mesage){
     $content = "\n\n\n-------------------------------------------\n".
                "Error on syncing sales \n".
                "Error message : ".$error_mesage.
-               "At : ".date("Y-M-d, H:i:s (D) e", time()) . "\n";    
+               "At : ".date("Y-M-d, H:i:s (D) e", time()) . "\n";
+    
     echo "<hr/>ERROR<br/>";
     print_r($content);
-    echo "<hr/>";    
+    echo "<hr/>";
+    
     Log::e($tag, $content); 
+//    Log::e($tag, "Login Responce: ".  multi_implode(" ", $json)); 
 }
 
 function login(){
@@ -123,37 +150,8 @@ function login(){
     }
 }
 
-function write_plu_file($stock){
-    $file_name = "C:\Users\Sakkeer Hussain\Desktop\mtch\plu.txt";
-    if(!file_exists(dirname($file_name))){
-        mkdir(dirname($file_name), 0777, true);
-    }
-    $handle = fopen($file_name, 'w');
-    fwrite($handle, get_plu_file_content($stock));
-    fclose($handle);
-}
 
 //utilities
-function get_plu_file_content($stock) {
-    $ret = '';
-    if(is_array($stock)){
-        foreach ($stock as $stock_item) {
-            if(is_array($stock_item)){
-                $ret .= $stock_item["item_id"].','
-                        .$stock_item["item_code"].','
-                        .$stock_item["item_name"].','
-                        .$stock_item["selling_prize"].','
-                        .$stock_item["tax_percentage"].','
-                        .$stock_item["stock_count"];              
-            }
-            $ret .= "\n";
-        }
-        return $ret;
-    }else{
-        return $array;
-    }
-}
-
 function multi_implode($glue, $array) {
     $ret = '';
     if(is_array($array)){
@@ -170,7 +168,10 @@ function multi_implode($glue, $array) {
         return $array;
     }
 }
+?>
 
+
+<?php
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
