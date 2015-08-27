60d74dc7e7c2f990b41f62a6495ce9a85eaf4d66
sakkeerhussain <sakkeerhussainp@gmail.com>
2015-08-27 14:46:21 +0530

    Update add_form_data.php

--- controller/api/add_form_data.php
+++ controller/api/add_form_data.php
@@ -2,7 +2,7 @@
 
 //registering for class auto loading
 spl_autoload_register(function($class_name) {
-    $controller_root = $_SERVER['DOCUMENT_ROOT'] . '/piknik_ims/controller';
+    $controller_root = $_SERVER['DOCUMENT_ROOT'] . '/online_ims/controller';
     if (file_exists($controller_root . '/php_classes/' . $class_name . '.php')) {
         $file_name = $controller_root . '/php_classes/' . $class_name . '.php';
         require_once $file_name;
